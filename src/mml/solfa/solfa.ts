bdf2fe6307005b3559fd78d73ba7efc0b8e96cdc
Mario Macias <mmacias@newrelic.com>
2020-04-25 19:57:22 +0200

    first nontested port of the solfa parser

--- src/mml/solfa/solfa.ts
+++ src/mml/solfa/solfa.ts
@@ -2,14 +2,15 @@ type Pitch = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G'
 type Halftone = '+' | '-'
 
 interface Note {
-    pitch?: Pitch
+    pitch?: Pitch // if no pitch, it's a silence
     halftone?: Halftone
     length: number // as a divisor. 1 == whole note
-    octave: number
+    octave?: number
+    tuplet?: number
 }
 
 interface ChannelStatus {
-    octave: 4
+    octave: number
 }
 
 const minOctave = 1
@@ -20,12 +21,12 @@ const maxLength = 64
 const defaultLength = 4
 
 class TabException {
-    tab: string
+    token: string
     position: number
     message: string
 
-    constructor(tab: string, position: number, message: string) {
-        this.tab = tab;
+    constructor(token: string, position: number, message: string) {
+        this.token = token;
         this.position = position;
         this.message = message;
     }
@@ -36,7 +37,7 @@ const tokenizer = new RegExp('^(' +
     '|([<>])' + // increase/decrease octave
     '|\s+|\|' + // spaces or vertical bars are ignored
     '|(\{[^\{}]*}\d+)' + // anything into brackets followed by a number: tuplet
-    +')').compile()
+    +')')
 
 function parse(tab: string): Note[] {
     return parseSubString(tab, {octave: defaultOctave})
@@ -49,10 +50,10 @@ function parseSubString(tab: string, status: ChannelStatus): Note[] {
     while (tab.length > 0) {
         const match = tokenizer.exec(tab)
         if (match == null) {
-            throw new TabException(wholeTab, index, `unexpected character '${tab[0]}'`)
+            throw new TabException(tab[0], index, 'unexpected character')
         }
-        tab = tab.substr(match.input.length)
-        const token = match.input
+        tab = tab.substr(match[0].length)
+        const token = match[0]
         switch (token[0].toLowerCase()) {
             case 'o':
                 parseOctave(index, token, status)
@@ -67,17 +68,17 @@ function parseSubString(tab: string, status: ChannelStatus): Note[] {
             case 'e':
             case 'f':
             case 'g':
-                notes.push(parseNote(index, token))
+                notes.push(parseNote(index, token, status))
                 break
             case '<':
                 if (status.octave <= minOctave) {
-                    throw new TabException(tab, index, `can't decrease octave below ${minOctave}`)
+                    throw new TabException(token, index, `can't decrease octave below ${minOctave}`)
                 }
                 status.octave--
                 break
             case '>':
                 if (status.octave >= maxOctave) {
-                    throw new TabException(tab, index, `can't increase octave over ${minOctave}`)
+                    throw new TabException(token, index, `can't increase octave over ${minOctave}`)
                 }
                 status.octave++
                 break
@@ -99,135 +100,94 @@ function parseSubString(tab: string, status: ChannelStatus): Note[] {
                 // just ignore
                 break
             default:
-                throw new TabException(tab, index, "unknown char ${tab[0]}")
+                throw new TabException(token, index, "unknown token")
         }
         index += token.length
     }
     return notes
 }
 
-function parseOctave(index: number, token: string, status: ChannelStatus) {
-
+const tuplet =  /^\{(.*)}(\d+)$/
+function parseTuplet(token: string, status: ChannelStatus): Note[] {
+    const sm = tuplet.exec(token)
+    try {
+        const notes = parseSubString(sm![1], status)
+        const nTuple: number = parseInt(sm![2], 10)
+        for (let i = 0 ; i < notes.length ; i++) {
+            notes[i].tuplet = nTuple
+        }
+        return notes
+    } catch(ex) {
+        if (ex instanceof TabException) {
+            ex.position++
+        }
+        throw ex
+    }
 }
 
 function parseSilence(index: number, token: string): Note {
-
+    if (token.length == 1) {
+        return { length: defaultLength }
+    }
+    const n = token.substring(1)
+    if (!/^\d+$/.test(n)) {
+        throw new TabException(token, index+1, "silence must be an R followed by a number")
+    }
+    return {length: parseInt(n, 10) }
 }
 
-function parseNote(index: number, token: string): Note {
+const pitches = new Map<string,Pitch>([
+    ['a', 'A'], ['A', 'A'],
+    ['b', 'B'], ['B', 'B'],
+    ['c', 'C'], ['C', 'C'],
+    ['d', 'D'], ['D', 'D'],
+    ['e', 'E'], ['E', 'E'],
+    ['f', 'F'], ['F', 'F'],
+    ['g', 'G'], ['G', 'G'],
+])
 
-
+const note = /^([+\-#]?)(\d*)$/
+function parseNote(index: number, token: string, status: ChannelStatus): Note {
+    const sm = note.exec(token.substring(1))
+    const n: Note = {
+        pitch: pitches.get(token.substring(0, 1)),
+        length: defaultLength,
+        octave: status.octave,
+    }
+    if (sm == null || sm.length === 0) {
+        return n
+    }
+    // note halftone
+    if (sm[1].length > 0) {
+        switch (sm[1]) {
+            case '+':
+            case '#':
+                n.halftone = '+'
+                break
+            case '-':
+                n.halftone = '-'
+                break
+        }
+    }
+    // note length
+    if (sm[2].length > 0) {
+        n.length = parseInt(sm[2])
+        if (n.length < minLength || n.length > maxLength) {
+            throw new TabException(token, index, `note length must be between ${minLength} and ${maxLength}`)
+        }
+    }
+    return n
 }
 
-function parseTuplet(token: string, status: ChannelStatus): Note[] {
-
-
+function parseOctave(index: number, token: string, status: ChannelStatus) {
+    if (!/^[Oo]\d+$/.test(token)) {
+        throw new TabException(token, index+1,
+            `Octave must have a number between ${minOctave} and ${maxOctave}`)
+    }
+    const o = parseInt(token.substring(1), 10)
+    if (o < minOctave || o > maxOctave) {
+        throw new TabException(token, index,
+            `Octave must have a number between ${minOctave} and ${maxOctave}`)
+    }
+    status.octave = o
 }
-
-
-/*
-
-var tuplet = regexp.MustCompile('^\{(.*)}(\d+)$')
-
-func parseTuplet(token []byte, c *channel) ([]Note, error) {
-	sm := tuplet.FindSubmatch(token)
-	if sm == nil {
-		panic(fmt.Sprintf("wrong format for tuplet: %q! this is a bug", string(token)))
-	}
-
-	tNotes, err := parseSubstring(sm[1], c)
-	if err != nil {
-		return nil, err
-	}
-
-	nTuple, err := strconv.Atoi(string(sm[2]))
-	if err != nil {
-		return nil, fmt.Errorf("tuple needs a proper number suffix: %w", err)
-	}
-
-	for i := range tNotes {
-		tNotes[i].Tuplet = uint(nTuple)
-	}
-
-	return tNotes, nil
-}
-
-func parseSilence(token []byte) (Note, error) {
-	n := Note{Pitch: Silence}
-	if len(token) == 1 {
-		n.Length = defaultLength
-		return n, nil
-	}
-	length, err := strconv.Atoi(string(token[1:]))
-	if err != nil {
-		return n, fmt.Errorf("wrong format for silence: %q. It must be an 'R' followed by a number", string(token))
-	}
-	n.Length = uint(length)
-	return n, nil
-}
-
-var note = regexp.MustCompile('^([+\-#]?)(\d*)$')
-
-func parseNote(token []byte, c *channel) (Note, error) {
-	sm := note.FindSubmatch(token[1:])
-	if sm == nil {
-		panic(fmt.Sprintf("wrong format for note: %q! this is a bug", string(token)))
-	}
-	n := Note{
-		Pitch:    getPitch(token[0]),
-		Length:   defaultLength,
-		Octave:   c.Octave,
-		Halftone: NoHalftone,
-	}
-	// read Length
-	if len(sm[2]) > 0 {
-		l, err := strconv.Atoi(string(sm[2]))
-		if err != nil {
-			panic(fmt.Sprintf("wrong length for note: %q! this is a bug. Err: %s",
-				string(token), err.Error()))
-		}
-		if l < minLength || l > maxLength {
-			return Note{}, fmt.Errorf(
-				"wrong note length: %d. Must be in range %d to %d", l, minLength, maxLength)
-		}
-		n.Length = uint(l)
-	}
-	// read halftone
-	if len(sm[1]) > 0 {
-		switch sm[1][0] {
-		case '+', '#':
-			n.Halftone = Sharp
-		case '-':
-			n.Halftone = Flat
-		default:
-			panic(fmt.Sprintf("wrong halftone '%c'! this is a bug", sm[1][0]))
-		}
-	}
-	return n, nil
-}
-
-func parseOctave(token []byte, c *channel) error {
-	i, err := strconv.Atoi(string(token[1:]))
-	if err != nil {
-		return err
-	}
-	if i < minOctave || i > maxOctave {
-		return fmt.Errorf("octave value must be 1 to 8")
-	}
-	c.Octave = uint(i)
-	return nil
-}
-
-var pitches = [8]Pitch{A, B, C, D, E, F, G}
-
-func getPitch(c byte) Pitch {
-	if c >= 'A' && c <= 'Z' {
-		return pitches[c-'A']
-	}
-	if c >= 'a' && c <= 'z' {
-		return pitches[c-'a']
-	}
-	panic(fmt.Sprintf("pitch can't be '%c'! this is a bug", c))
-}
-
- */
